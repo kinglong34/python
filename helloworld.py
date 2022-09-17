184e202e82a4607682e580966211be441dccf479
Kwan King Long <kinglongk@yahoo.com.hk>
2022-09-17 15:24:16 +0800

    open_chrome

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,5 @@
-msg = "HelloWorld"
-#try to commit
-print(msg)
+from selenium import webdriver
+
+# 開啟瀏覽器視窗(Chrome)
+# 方法一：執行前需開啟chromedriver.exe且與執行檔在同一個工作目錄
+driver = webdriver.Chrome("/Users/kinglongkwan/Documents/GitHub/python/chromedriver")
